c6bb6bc72096aab30bcbe31657b08a618f777951
abhay-sharma46093 <abhay.sharma@infostride.com>
2023-02-15 15:57:03 +0530

    project setup

---
+++ src/main/java/com/infostride/springbootpostgress/SpringbootPostgressApplication.java
@@ -0,0 +1,13 @@
+package com.infostride.springbootpostgress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infostride/springbootpostgress/SpringbootPostgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infostride.springbootpostgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
